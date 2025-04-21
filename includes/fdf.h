ece00104deb9d3c642d48311a04c165fcd448d0f
jdupuis <jdupuis@student.42perpignan.fr>
2025-04-21 14:53:55 +0200

    auto/dev

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,15 +6,13 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 14:00:55 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/04/21 14:37:20 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/04/21 14:53:52 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-#include "../lib/LibFT/Includes/libft.h"
-#include <stdio.h>
 #include <mlx.h>
 
 #endif
