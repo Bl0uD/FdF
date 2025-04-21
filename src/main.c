255f2998fd102d91021559ddd618a58084995912
jdupuis <jdupuis@student.42perpignan.fr>
2025-04-21 14:50:35 +0200

    auto/dev

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 14:01:20 by jdupuis           #+#    #+#             */
-/*   Updated: 2025/04/21 14:41:38 by jdupuis          ###   ########.fr       */
+/*   Updated: 2025/04/21 14:50:19 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 int	main(void)
 {
 	void	*mlx;
+	void	*mlx_win;
 
 	mlx = mlx_init();
-	(void)mlx;
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
